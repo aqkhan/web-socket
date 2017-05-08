52f9f51a1fe9a951866c7dd80bb8a4a300db7c46
A Q Khan <aqk.neo@gmail.om>
2017-05-08 11:43:17 +0500

    Lift off

---
+++ app.js
@@ -0,0 +1,11 @@
+// Require WS module
+var webSocketServer = require('ws').Server();
+
+// Create web socket server
+var wss = new webSocketServer({port: 3000});
+// Another example: const ws = new WebSocket('ws://www.host.com/path');
+
+// On establishing connection
+wss.on('connection', function(ws){
+    ws.send("Connection established");
+});
